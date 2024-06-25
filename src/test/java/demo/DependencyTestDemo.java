faca6ac08f60f37dca9a09c7c9fd60b9e4643ac6
Kenan Mammadli <kenan@doe.org>
2024-06-25 12:41:25 -0400

    dependency in methods, regex xml

---
+++ src/test/java/demo/DependencyTestDemo.java
@@ -0,0 +1,47 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.List;
+
+public class DependencyTestDemo {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void testValidLogin(){
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester", Keys.TAB,
+                "testWrongPart", Keys.ENTER);
+        Assert.assertEquals(driver.getTitle(), "Web Orders");
+    }
+
+    @Test (dependsOnMethods = "testValidLogin")
+    public void testDeleteSelected() throws InterruptedException {
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
+        for (WebElement checkbox : checkboxes) {
+            checkbox.click();
+        }
+        driver.findElement(By.id("ctl00_MainContent_btnDelete")).click();
+        Thread.sleep(1000);
+        Assert.assertTrue(driver.getPageSource().contains("List of orders is empty. In order to add new order use"));
+    }
+}
---
+++ src/test/java/demo/TestingRegex.java
@@ -0,0 +1,34 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class TestingRegex {
+
+    @Test
+    public void test_001_UI(){
+        System.out.println("A");
+    }
+
+    @Test
+    public void test_002_UI(){
+        System.out.println("B");
+    }
+
+    @Test
+    public void test_003_UI(){
+        System.out.println("C");
+    }
+
+    @Test
+    public void tc_001_API(){
+        System.out.println("D");
+    }
+
+    @Test
+    public void tc_002_API(){
+        System.out.println("E");
+    }
+
+}
+
+
